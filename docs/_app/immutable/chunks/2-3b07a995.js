a95d8f7395351d0dc9c08f1860884c71cbd7c00d
Matt <matt@matts-mbp.mynetworksettings.com>
2023-03-23 15:33:16 -0400

    update github pages

--- docs/_app/immutable/chunks/2-3b07a995.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-63f99d9b.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-5bbb6847.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-cce2480e.js";export{t as component};
